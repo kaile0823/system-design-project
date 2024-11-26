866319b9d3d35503bc61c41a3361570210ce9894
Nut17 <howwilson11@gmail.com>
2024-11-26 22:27:11 +0800

    Update after merge

--- backend/app.js
+++ backend/app.js
@@ -2,19 +2,29 @@
 import express from 'express';
 import cors from 'cors';
 
-import { connectMongoDB } from './config/dbMongo.js';
+// import { connectMongoDB } from './config/dbMongo.js';
 import { connectSqlite } from './config/dbSqlite.js';
+import { sequelize } from './config/dbSqlite.js';
 
 import userRoutes from './routes/userRoutes.js';
 import productRoutes from './routes/productRoutes.js';
 import settingRoutes from './routes/settingRoutes.js';
 import imageRoutes from './routes/imageRoutes.js';
+import itemRoutes from './routes/itemRoutes.js';
 
 const app = express();
 
 // Connect to databases
 // connectMongoDB();
 connectSqlite();
+
+sequelize.sync({ force: false }) // 設置為 true 將重置資料表，開發時慎用
+    .then(() => {
+        console.log('Database & tables created!');
+    })
+    .catch(error => {
+        console.error('Error syncing database:', error);
+    });
 
 // Middleware
 app.use(express.json());
@@ -27,5 +37,5 @@ app.use('/api', userRoutes);
 app.use('/api', productRoutes);
 app.use('/api', settingRoutes);
 app.use('/api', imageRoutes);
-
+app.use('/api', itemRoutes);
 export default app;
--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,4 @@
-import express from 'express'; // 替換 require 為 import
-import { sequelize } from './config/dbSqlite.js'; // 使用具名匯入
-import itemRoutes from './routes/itemRoutes.js'; // 替換路由為 ESM
-
-const app = express(); // 初始化 Express 應用
-
-app.use(express.json()); // 使用中介軟體解析 JSON 請求
-
-// 使用路由
-app.use('/api', itemRoutes); // 所有 /api 的請求會轉交給 itemRoutes
-
-// 同步資料庫
-sequelize.sync({ force: false }) // 設置為 true 將重置資料表，開發時慎用
-    .then(() => {
-        console.log('Database & tables created!');
-    })
-    .catch(error => {
-        console.error('Error syncing database:', error);
-    });
+import app from "./app.js";
 
 const PORT = 3002;
 app.listen(PORT, () => {
