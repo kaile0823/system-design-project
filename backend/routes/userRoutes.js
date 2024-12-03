d5c67a836ed8f1359bd390fcb1a68410a5b22e07
Nut17 <howwilson11@gmail.com>
2024-12-03 17:35:39 +0800

    Cleaning repository tree

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getUsersController, getUserController, addUserController, updateUserController, deleteUserController } from '../controllers/userController.js';
+import { getUsersController, getUserController, addUserController, updateUserController, deleteUserController, loginController, resetPasswordController } from '../controllers/userController.js';
 
 const router = express.Router();
 
@@ -8,5 +8,8 @@ router.get('/users/:id', getUserController);
 router.post('/users', addUserController);
 router.put('/users/:id', updateUserController);
 router.delete('/users/:id', deleteUserController);
+
+router.post('/users/login', loginController);
+router.post('/users/reset-password', resetPasswordController);
 
 export default router;
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,7 +6,27 @@ axios.defaults.headers.post['Content-Type'] = 'application/json';
 </script>
 
 <template>
-    <main class="inherit-dimension">
-        <router-view />
-    </main>
+  <main class="inherit-dimension">
+    <!-- <transition name="fade" mode="out-in">
+            <router-view />
+        </transition> -->
+    <router-view v-slot="{ Component }">
+      <transition name="fade" mode="out-in">
+        <component :is="Component" />
+      </transition>
+    </router-view>
+  </main>
 </template>
+
+<style>
+/* Add transition effect */
+.fade-enter-active,
+.fade-leave-active {
+  transition: transform 0.5s ease;
+}
+
+.fade-enter,
+.fade-leave-to {
+  transform: translateX(100%);
+}
+</style>
